460d3349fc34956e8733940152a4325e82cd25de
deborahferraz <ferrazpontesdeborah@gmail.com>
2024-11-17 22:46:11 -0300

    Create Esmalte.js

---
+++ Esmalte.js
@@ -0,0 +1,13 @@
+class Esmalte {
+    constructor(Id, marca, nome, cor, tipoDeEsmalte, dataValidade, quantasVezesUsado, posicaoNaLista) {
+        this.Id = Id;
+        this.marca = marca;
+        this.nome = nome;
+        this.cor = cor;
+        this.tipoDeEsmalte = tipoDeEsmalte;
+        this.dataValidade = dataValidade;
+        this.quantasVezesUsado = quantasVezesUsado;
+
+        this.posicaoNaLista = posicaoNaLista;
+    }
+}
